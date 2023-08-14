705c21fb67df41af54b3a3d36f694704a586f4a7
piggywave <bisaraver@gmail.com>
2023-08-14 19:59:46 +0800

    feat: add keypair support

--- hooks/00-hook.py
+++ hooks/00-hook.py
@@ -16,6 +16,13 @@ hooks = '''
         "kind": "RSA",
         "executeHookOnEvent":["Added", "Deleted"],
         "executeHookOnSynchronization": false
+    },
+    {
+        "name": "monitoring keypair",
+        "apiVersion": "stable.example.com/v1",
+        "kind": "KeyPair",
+        "executeHookOnEvent":["Added", "Deleted"],
+        "executeHookOnSynchronization": false
     }
   ]
 }
@@ -54,7 +61,34 @@ class RSARequest:
                     except client.ApiException as e:
                         print(e)
 
+class KeyPair:
+    def __init__(self, data: Dict[str, any]) -> None:
+        self.mode = data['watchEvent']
+        self.name = data['object']['spec']['name']
+        self.namespace = data['object']['spec']['namespace']
+        self.pairs = data['object']['spec']['data']
 
+    def keypair_gen(self):
+        config.load_config()
+        v1 = client.CoreV1Api()
+        match self.mode:
+            case "Added":
+                obj = client.V1Secret(api_version="v1", metadata={"name": self.name})
+                obj.data = {}
+                for key, value in self.pairs.items():
+                    obj.data[key] = base64.b64encode(value.encode()).decode()
+                for ns in self.namespace:
+                    try:
+                        obj.namespace = ns
+                        v1.create_namespaced_secret(namespace=ns, body=obj)
+                    except client.ApiException as e:
+                        print(e)
+            case "Deleted":
+                for ns in self.namespace:
+                    try:
+                        v1.delete_namespaced_secret(name=self.name, namespace=ns)
+                    except client.ApiException as e:
+                        print(e)
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "--config":
         print(hooks)
@@ -63,8 +97,13 @@ def main():
         with open(os.environ.get("BINDING_CONTEXT_PATH"), 'r') as f:
             events = json.load(f)
             for event in events:
-                rr = RSARequest(event)
-                rr.rsa_gen()
+                match event['binding']:
+                    case "monitoring rsa":
+                        rr = RSARequest(event)
+                        rr.rsa_gen()
+                    case "monitoring keypair":
+                        kp = KeyPair(event)
+                        kp.keypair_gen()
 
 if __name__ == '__main__':
     main()
