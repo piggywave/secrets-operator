4c14f915b4df4e7b0a241b90e5ae8cbc7082ed94
piggywave <bisaraver@gmail.com>
2023-08-14 12:34:07 +0800

    refactor: rsa gen

--- hooks/00-hook.py
+++ hooks/00-hook.py
@@ -3,70 +3,68 @@
 import sys, base64, os, json
 import rsa
 from kubernetes import config, client
-from typing import List
+from typing import Dict
 
 
-rsa_hook = '''
+hooks = '''
 {
   "configVersion":"v1",
-  "kubernetes":[{
-    "name": "monitoring rsa",
-    "apiVersion": "stable.example.com/v1",
-    "kind": "RSA",
-    "executeHookOnEvent":["Added", "Deleted"],
-    "executeHookOnSynchronization":false
-  }]
+  "kubernetes":[
+    {
+        "name": "monitoring rsa",
+        "apiVersion": "stable.example.com/v1",
+        "kind": "RSA",
+        "executeHookOnEvent":["Added", "Deleted"],
+        "executeHookOnSynchronization": false
+    }
+  ]
 }
 '''
 
-def rsa_gen(name: str, namespace: List[str], length: int, mode: str, pubkey: str, prikey: str):
-    config.load_config()
-    v1 = client.CoreV1Api()
-    match mode:
-        case "Added":
-            pub, pri = rsa.newkeys(length)
-            obj = client.V1Secret(api_version="v1", metadata={"name": name})
-            obj.data = {
-                pubkey: base64.b64encode(pub.save_pkcs1()).decode(),
-                prikey: base64.b64encode(pri.save_pkcs1()).decode()
-            }
-            for ns in namespace:
-                try:
-                    obj.namespace = ns
-                    v1.create_namespaced_secret(namespace=ns, body=obj)
-                except client.ApiException as e:
-                    if e.status == 409:
-                        log = "%s/%s already exists".format(ns, name)
-                        print(log)
-                    else:
+class RSARequest:
+    def __init__(self, data: Dict[str, any]) -> None:
+        self.mode = data['watchEvent']
+        self.name = data['object']['spec']['name']
+        self.namespace = data['object']['spec']['namespace']
+        self.length = data['object']['spec']['length']
+        self.public_key = data['object']['spec']['publicKey']
+        self.private_key = data['object']['spec']['privateKey']
+
+    def rsa_gen(self):
+        config.load_config()
+        v1 = client.CoreV1Api()
+        match self.mode:
+            case "Added":
+                pub, pri = rsa.newkeys(self.length)
+                obj = client.V1Secret(api_version="v1", metadata={"name": self.name})
+                obj.data = {
+                    self.public_key: base64.b64encode(pub.save_pkcs1()).decode(),
+                    self.private_key: base64.b64encode(pri.save_pkcs1()).decode()
+                }
+                for ns in self.namespace:
+                    try:
+                        obj.namespace = ns
+                        v1.create_namespaced_secret(namespace=ns, body=obj)
+                    except client.ApiException as e:
                         print(e)
-        case "Deleted":
-            for ns in namespace:
-                try:
-                    v1.delete_namespaced_secret(name=name, namespace=ns)
-                except client.ApiException as e:
-                    if e.status == 404:
-                        log = "%s/%s does not exist".format(ns, name)
-                        print(log)
-                    else:
+            case "Deleted":
+                for ns in self.namespace:
+                    try:
+                        v1.delete_namespaced_secret(name=self.name, namespace=ns)
+                    except client.ApiException as e:
                         print(e)
 
 
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "--config":
-        print(rsa_hook)
+        print(hooks)
         return
     else:
         with open(os.environ.get("BINDING_CONTEXT_PATH"), 'r') as f:
-            data = json.load(f)
-            for event in data:
-                event_type = event['watchEvent']
-                name = event['object']['spec']['name']
-                namespace = event['object']['spec']['namespace']
-                length = event['object']['spec']['length']
-                publickey = event['object']['spec']['publicKey']
-                privatekey = event['object']['spec']['privateKey']
-                rsa_gen(name, namespace, length, event_type, publickey, privatekey)
+            events = json.load(f)
+            for event in events:
+                rr = RSARequest(event)
+                rr.rsa_gen()
 
 if __name__ == '__main__':
     main()
